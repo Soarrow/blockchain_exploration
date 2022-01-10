c40482392f1e58b89c8131455e427bcc8feb4a73
WenBo <wenbo.zhao.1998@gmail.com>
2022-01-10 23:55:34 +0000

    added code to new_transaction

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,10 @@
 class Blockchain(object):
+    # Responsible for managing the chain
+    # Will store transactions and have helper methods for adding new blocks to the chain
+
     def __init__(self):
         # used to store our blockchain
-        self.contain = []
+        self.chain = []
 
         # used to store transactions
         self.current_transactions = []
@@ -10,9 +13,23 @@ class Blockchain(object):
         # Creates a new Block and adds it too the chain
         pass
     
-    def new_transaction(self):
+    def new_transaction(self, sender, recipient, amount):
         # Adds a new transaction to the list of transcations
-        pass
+        # Way of adding transactions to a Block
+        # Creates a new transaction to go into the next mined Block
+        # :param sender: <str> Address of the Sender
+        # :param recipient: <str> Address of the recipient
+        # :param amount: <int> Amount
+        # :return: <int> the index of theo Block that will hold this transaction
+
+        self.current_transactions.append({
+            'sender' : sender,
+            'recipient' : recipient,
+            'amount' : amount,
+        })
+
+        # returns the index the block which the transactions will be added to (the next one to be mined)
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
